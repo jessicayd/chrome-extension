0117958357dee714f74c1abd1a68dce7023132cc
Jessica Dong <jd9751@princeton.edu>
2023-08-06 18:05:47 -0400

    icons

--- script.js
+++ script.js
@@ -21,3 +21,43 @@ function setTime(){
 setInterval(function() {
     setTime();
   }, 500)
+
+// Function to create an icon
+function createIcon(url) {
+    let iconContainer = document.getElementById('icons');
+
+    let iconWrapper = document.createElement('div');
+    iconWrapper.className = 'icon-wrapper';
+
+    let link = document.createElement('a');
+    link.className = 'icon-link';
+    link.href = url;
+
+    let icon = document.createElement('img');
+    icon.src = 'images/weather.jpeg';
+    icon.className = 'icon-image';
+    icon.alt = 'icon'; 
+
+    let edit = document.createElement('button');
+    edit.className = 'edit-button';
+    edit.textContent = 'edit';
+
+    // Add an event listener for the "Edit URL" button
+    edit.addEventListener('click', function (event) {
+        event.preventDefault();
+        let newLink = prompt('Edit link: ');
+        if (newLink) {
+            link.href = newLink;
+        }
+    });
+
+    iconWrapper.appendChild(link);
+    iconWrapper.appendChild(edit);
+    link.appendChild(icon);
+    iconContainer.appendChild(iconWrapper);
+}
+
+// Call the createIcon function with different URLs to create multiple icons
+createIcon('https://www.google.com/search?q=weather+28226');
+createIcon('https://github.com/jessicayd/chrome-extension');
+createIcon('https://google.com');
