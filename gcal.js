b3f0e7a6db854b1c5b97bac1f2d637d3c7f39006
serenahwang12 <106443799+serenahwang12@users.noreply.github.com>
2023-08-09 16:20:03 -0400

    gcal fixes

--- gcal.js
+++ gcal.js
@@ -1,39 +1,76 @@
-  
-  // gcal.js
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById('gcal-signin').addEventListener('click', function() {
+    chrome.identity.getAuthToken({interactive: true}, function(token) {
+      console.log("Authentication successful. Token:", token);
 
-async function listUpcomingEvents(token) {
-    let response;
-    try {
-      const request = {
-        'calendarId': 'primary',
-        'timeMin': (new Date()).toISOString(),
-        'showDeleted': false,
-        'singleEvents': true,
-        'maxResults': 10,
-        'orderBy': 'startTime',
+      var init = {
+        method: 'GET',
+        async: true,
+        headers: {
+          Authorization: 'Bearer ' + token,
+          'Content-Type': 'application/json'
+        },
+        'contentType': 'json'
       };
+
+      const now = new Date();
+      const isoNow = now.toISOString();
+
+      const maxResults = 5;
+
+      const eventsUrl = `https://www.googleapis.com/calendar/v3/calendars/primary/events?timeMin=${isoNow}&maxResults=${maxResults}`;
       
-      // Use the access token for the authorized API request.
-      response = await gapi.client.calendar.events.list({
-        ...request,
-        'headers': {
-          'Authorization': `Bearer ${token}`
-        }
-      });
-    } catch (err) {
-      document.getElementById('content').innerText = err.message;
-      return;
-    }
+      fetch(eventsUrl, init)
+        .then((response) => response.json())
+        .then((data) => {
+          if (data.items) {
+            // Loop through the upcoming events and display them.
+            data.items.forEach((event) => {
+              console.log("Event:", event.summary, "Start:", event.start.dateTime);
+              // You can display the event details in your extension's UI.
+            });
+          } else {
+            console.log("No upcoming events found.");
+          }
+        })
+        .catch((error) => {
+          console.error("Error fetching events:", error);
+        });
+
+    });
+  });
+
+});
+
+
+// Function to fetch upcoming events using the provided token.
+function fetchUpcomingEvents(token) {
+  const calendarId = "primary"; // You can change this to the desired calendar ID.
+  const maxResults = 3; // Number of upcoming events to fetch.
+
+  // API endpoint URL to fetch events.
+  // const eventsUrl = `https://www.googleapis.com/calendar/v3/calendars/${calendarId}/events?maxResults=${maxResults}&orderBy=startTime`;
+  const eventsUrl = "https://www.googleapis.com/calendar/v3/calendars/primary"
   
-    const events = response.result.items;
-    if (!events || events.length == 0) {
-      document.getElementById('content').innerText = 'No events found.';
-      return;
-    }
-    // Flatten to string to display
-    const output = events.reduce(
-        (str, event) => `${str}${event.summary} (${event.start.dateTime || event.start.date})\n`,
-        'Events:\n');
-    document.getElementById('content').innerText = output;
-  }
-  
+  // Fetch events using the token.
+  fetch(eventsUrl, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.items) {
+        // Loop through the upcoming events and display them.
+        data.items.forEach((event) => {
+          console.log("Event:", event.summary, "Start:", event.start.dateTime);
+          // You can display the event details in your extension's UI.
+        });
+      } else {
+        console.log("No upcoming events found.");
+      }
+    })
+    .catch((error) => {
+      console.error("Error fetching events:", error);
+    });
+}
