6d9a9e66fb4ee84c003763bbe635c543520e00f4
Jessica Dong <jessicad.7373@gmail.com>
2023-08-09 03:50:28 -0400

    more stylish typetest

--- type.js
+++ type.js
@@ -4,21 +4,22 @@ text = document.getElementById('typing-quote'),
 time = document.getElementById('timer'),
 wpm = document.getElementById('score'),
 acc = document.getElementById('accuracy'),
-errors = document.getElementById('errors'),
-charCount = input.value.length;
+count = document.getElementById('count'),
+errors = document.getElementById('errors');
 
 let totalTime = 0,
 quoteChars = [],
 mistakes = 0,
 isTimerStarted = false,
-startTime = 0, intervalId = null;
+startTime = 0, intervalId = null, quoteLength = 0;
+let hasFailed = false;
 
 // gets paragraph and sets initializing stuff for reset
 const loadQuote = async () => {
     const response = await fetch("./quotes.json");
     let data = await response.json();
     quote = data[Math.floor(Math.random() * data.length)].text; // random quote kinda
-    
+
     // initial
     text.innerHTML = "";
     input.value = "";
@@ -33,6 +34,9 @@ const loadQuote = async () => {
     });
     quoteChars = text.querySelectorAll('span');
     quoteChars = Array.from(quoteChars);
+
+    quoteLength = quoteChars.length;
+    count.innerText = quoteLength;
   };
 
 // don't paste into the box D:
@@ -50,38 +54,44 @@ input.addEventListener('input', function() {
 
     let inputChars = input.value.split("");
 
-    let hasFailed = false;
     quoteChars.forEach((char, index) => {
-        
+        console.log(char);
         if (char.innerText == inputChars[index]) {
             char.classList.add("success");
+            char.style.color = "rgb(70, 171, 87)";
         }
         
         // backspace
         else if (inputChars[index] == null) {
-            if (char.classList.contains("success")) char.classList.remove("success");
-            else {
+            if (char.classList.contains("success")) {
+                char.classList.remove("success");
+                char.style.color = "rgb(117, 95, 87)";
+            }
+            else if (char.classList.contains("fail")) {
                 char.classList.remove("fail");
-                hasFailed = true;
+                hasFailed = false; 
+                input.style.color = "rgb(133, 114, 107)";
             }
         }
         
         else {
-            if (!char.classList.contains("fail") && !hasFailed) {
-                mistakes += 1;
-                char.classList.add("fail");
-                hasFailed = true;
+            if (!char.classList.contains("fail")) {
+                if (!hasFailed) {
+                    hasFailed = true;
+                    mistakes += 1;
+                    char.classList.add("fail");
+                    input.style.color = "rgb(247, 106, 106)";
+                }
             }
             errors.innerText = mistakes;
         }
-        //Returns true if all the characters are entered correctly
+        
         let check = quoteChars.every((element) => {
             return element.classList.contains("success");
         });
-        //End test if all characters are correct
+        
         if (check) {
             input.disabled = true;
-    
             displayResult();
         }
     });
@@ -101,8 +111,12 @@ const updateTimer = () => {
     const seconds = Math.floor((elapsedTime % 60000) / 1000);
     const deciseconds =  Math.floor((elapsedTime % 1000) / 100);
     totalTime = minutes + seconds/60 + deciseconds / 600;
-    wpm.innerText = (charCount / 5 / totalTime).toFixed(0);
-    acc.innerText = (charCount - errors) / charCount * 100;
+    if (!hasFailed) {
+        let charCount = input.value.length;
+        wpm.innerText = (charCount / 5 / totalTime).toFixed(0);
+        acc.innerText = ((charCount - mistakes) / charCount * 100).toFixed(0);
+        count.innerText = quoteLength - charCount;
+    }
 
     const formattedTime = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}.${deciseconds}`;
     time.innerText = formattedTime;
@@ -117,8 +131,10 @@ const stopTimer = () => {
 const displayResult = () => {
     stopTimer();
     input.disabled = true;
-    wpm.innerText = (input.value.length / 5 / totalTime).toFixed(2);
-    acc.innerText = (charCount - errors) / charCount * 100;
+    let charCount = input.value.length;
+    wpm.innerText = (charCount / 5 / totalTime).toFixed(2);
+    acc.innerText = ((charCount - mistakes) / charCount * 100).toFixed(2);
+    count.innerText = quoteLength - charCount;
 };
 
 // try again
