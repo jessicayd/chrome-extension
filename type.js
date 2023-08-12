9c099e36494026bfa3d58efa492dd0bf869b2693
Jessica Dong <jessicad.7373@gmail.com>
2023-08-12 03:38:19 -0400

    changing quotes

--- type.js
+++ type.js
@@ -37,9 +37,9 @@ const loadQuote = async () => {
   };
 
 // don't paste into the box D:
-// input.addEventListener('paste', (event) => {
-//     event.preventDefault(); 
-// });
+input.addEventListener('paste', (event) => {
+    event.preventDefault(); 
+});
 
 // handles when u type
 input.addEventListener('input', function() {
